3697ae5a76146f80316cf24a32823e141b154998
unknown <yagensh16697@gmail.com>
2022-11-29 23:49:46 +0530

    py exercises

---
+++ python/exercises.py
@@ -0,0 +1,31 @@
+# 1 normal
+# l = []
+
+# for i in range(2000, 3201):
+#     if (i % 7 == 0) and (i % 5 != 0):
+#         l.append(str(i))
+
+# # print(",".join(l))
+# print(','.join(l))
+
+
+# 2 factorial
+
+# def fact(num):
+#     if (num == 0):
+#         return 1
+#     return num * fact(num-1)
+
+# x = int(input())
+# print(fact(x))
+
+
+# 3 ip: 8 op: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+
+# x = int(input())
+# d = dict()
+
+# for i in range(1, x+1):
+#     d[i] = i*i
+
+# print(d)
